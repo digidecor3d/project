120abcf9466e4845bdc94a165236b97b25786496
digidecor3d <45209978+digidecor3d@users.noreply.github.com>
2019-10-30 18:31:05 +0330

    9

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.listen('8000',function () {
     console.log("server runing at http://localhost:7000")
 })
 app.use('/',function (req,res,next) {
-   console.log('hello world')
+   console.log('hello world999')
     next()
 })
 app.use('/',function (req,res,next) {
