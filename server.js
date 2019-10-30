5bf06513c9c1832bc773528b3b242b35de4c5e43
digidecor3d <45209978+digidecor3d@users.noreply.github.com>
2019-10-30 13:43:25 +0330

    create

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express()
+
+
+app.listen('8000',function () {
+    console.log("server runing at http://localhost:7000")
+})
+app.use('/',function (req,res,next) {
+   console.log('hello world')
+    next()
+})
+app.get('/',function (req,res) {
+    res.send('hello world')
+})
